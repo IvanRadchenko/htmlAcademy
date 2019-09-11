0ae8ac60b845c62009c31bcec2577ed9ebc648d6
Ivan Radchenko <radchenk@gmail.com>
2019-09-11 19:47:27 +0300

    Fix logo svg issues

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import  './styles/index.scss'
 import './js/app'
 import galleryImg from './images/index-gallery.jpg'
 
-
-// Append SVG and heading nodes to the DOM
-const app = document.querySelector('.nav-logo__media');
-app.src = indexLogo;
-
-
 const gallery = document.querySelector('.block__container_gallery');
 Object.values(gallery).forEach( (img) => img.src = galleryImg);
 
--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,8 @@
 import logoIndex from '../images/index-logo.svg'
 import logoCommon from '../images/logo.svg'
+import loginSVG from '../images/login.svg'
 
-const logo = document.querySelector(".nav-logo__media");
+const logo = document.querySelector('.nav-logo__media');
 
 if (location.pathname === '/index.html') {
   logo.src = logoIndex;
@@ -9,3 +10,6 @@ if (location.pathname === '/index.html') {
   logo.src = logoCommon;
 }
 
+
+const loginLinkImage = document.querySelector('.login-link:before');
+loginLinkImage.style.backgroundImage = loginSVG;
